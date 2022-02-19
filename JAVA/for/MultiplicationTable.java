928ee035d2036b39e236e11cbfd6ca865b38f301
harandal24601 <devchaehokim@gmail.com>
2022-02-20 00:03:02 +0900

    wrote few extra code

--- JAVA/for/MultiplicationTable.java
+++ JAVA/for/MultiplicationTable.java
@@ -4,6 +4,10 @@ public class MultiplicationTable {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         int low = scanner.nextInt();
+        scanner.close();
 
+        for (int i = 1; i < 10; i++) {
+            System.out.println(low + " * " + " = " + (low*i));
+        }
     }
 }
