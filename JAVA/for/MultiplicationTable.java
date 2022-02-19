af15e685328bddab9bc361da5c67a3ed7ce55733
harandal24601 <devchaehokim@gmail.com>
2022-02-19 23:36:51 +0900

    add for directory

---
+++ JAVA/for/MultiplicationTable.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int low = scanner.nextInt();
+
+    }
+}
