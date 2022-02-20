292d12ae9f64bf0cfaa1cfd7149ab20f452128f8
harandal24601 <devchaehokim@gmail.com>
2022-02-21 03:42:48 +0900

    add algorithm

---
+++ JAVA/for/PrintCaseNum.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class PrintTestNum{
+    public static void main(String[] args)throws IOException{
+	BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	int t = Integer.parseInt(bf.readLine());
+	StringTokenizer st;
+	
+	for(int i = 1; i <= t; i++){
+	    st = new StringTokenizer(bf.readLine());
+	    bw.write("Case #" + i + ": " + (Integer.parseInt(st.nextToken())+Integer.parseInt(st.nextToken())) + "\n");
+	}
+	bw.flush();
+	bw.close();
+    }
+}
