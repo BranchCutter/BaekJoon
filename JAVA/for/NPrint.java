3e8790371dd4f0b4380396ac376079a3b013a288
harandal24601 <devchaehokim@gmail.com>
2022-02-21 03:20:20 +0900

    add algorithm

---
+++ JAVA/for/NPrint.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class NPrint{
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = n; i >= 1; i--){
+            bw.write(i + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+}
