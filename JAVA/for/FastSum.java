424d0427477844e6fa300518b3c887f84501c47b
harandal24601 <devchaehokim@gmail.com>
2022-02-20 20:52:14 +0900

    new java file using BufferedReader function

---
+++ JAVA/for/FastSum.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class FastSum{
+    public static void main(String[] args) throws IOException {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	StringTokenizer st;
+	int testCase = Integer.parseInt(br.readLine());
+	for(int i = 0; i < testCase; i++) {
+	    st = new StringTokenizer(br.readLine());
+	    bw.write((Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken())) + "\n");
+	}
+	bw.close();
+    }
+}
