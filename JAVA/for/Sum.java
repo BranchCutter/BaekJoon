de6ee41c4162e477cd5a9a2b7dbd5abbffdda908
harandal24601 <devchaehokim@gmail.com>
2022-02-20 15:39:13 +0900

    add more algorithm on for directory

---
+++ JAVA/for/Sum.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Sum{
+    public static void main(String[] args){
+	Scanner scanner = new Scanner(System.in);
+	int sum = 0;
+	int n = scanner.nextInt();
+	scanner.close();
+	
+	for(int i = 1; i <= n; i++){
+		sum += i;
+	}
+	System.out.println(sum);
+    }
+}
---
+++ JAVA/for/TestCase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TestCase{
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+		int testCase = scanner.nextInt();
+		
+		for (int i = 0; i < testCase; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+			System.out.println(a+b);
+		}
+		scanner.close();
+	}
+}
