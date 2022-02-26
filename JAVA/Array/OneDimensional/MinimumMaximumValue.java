bd8cc657b3335e467317a6002e1559693ad7532b
harandal24601 <devchaehokim@gmail.com>
2022-02-26 22:24:55 +0900

    feat : add algorithm using array

---
+++ JAVA/Array/OneDimensional/MinimumMaximumValue.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MinimumMaximumValue{
+    public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	int n = Integer.parseInt(br.readLine());
+	StringTokenizer st = new StringTokenizer(br.readLine()," ");
+	int[] arr = new int[n];
+	for (int i = 0; i <= n-1; i++){
+	    arr[i] = Integer.parseInt(st.nextToken());
+	}
+	Arrays.sort(arr);
+	System.out.println(arr[0] + " " + arr[n-1]);
+    }
+}
