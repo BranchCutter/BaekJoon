cf933e2640c725258d22d0811a94c59aa173fafc
harandal24601 <devchaehokim@gmail.com>
2022-02-27 23:15:48 +0900

    chore : add algorithm using array

---
+++ JAVA/Array/OneDimensional/LeftOver.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LeftOver{
+    public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	boolean[] arr = new boolean[42];
+	int cnt = 0;
+	
+	for(int i = 0 ; i < 10 ; i++){
+	    arr[Integer.parseInt(br.readLine()) % 42] = true;
+	}
+	
+	for(boolean value : arr){
+	    if(value){
+		cnt++;
+	    }
+	}
+	System.out.println(cnt);
+    }
+}	
