4211d40a47321b4ca89e8f3470d7f8451bd7c7c4
harandal24601 <devchaehokim@gmail.com>
2022-02-28 16:19:47 +0900

    feat : BaekJoon algorithm added(4344)

---
+++ JAVA/Array/OneDimensional/StatisticsOverAverage.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class StaticsOverAverage{
+    public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	int testCase = Integer.parseInt(br.readLine());
+	StringTokenizer st;
+	int[] arr;
+	double sum = 0;
+	
+	for(int i = 1 ; i <= testCase ; i++){
+	    st = new StringTokenizer(br.readLine()," ");
+	    int studentNum = Integer.parseInt(st.nextToken());
+	    arr = new int[studentNum];
+	    for(int j = 0 ; j < studentNum ; j++){
+		arr[j] = Integer.parseInt(st.nextToken());
+		sum += arr[j];
+	    }
+	    double average = (sum / studentNum);
+	    double cnt = 0;
+	    for(int j = 0 ; j < studentNum ; j++){
+		if(arr[j] > average) {
+		    cnt++;
+		}
+	    }
+	    bw.write(String.format("%.3f",(cnt / studentNum)*100) + "%\n");
+	    sum = 0;
+	}
+	bw.flush();
+	bw.close();
+    }
+}
