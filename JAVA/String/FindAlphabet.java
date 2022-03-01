336a724c2d3a94c204276603107b4e93b30bc5bf
harandal24601 <devchaehokim@gmail.com>
2022-03-01 18:25:17 +0900

    chore : file update

---
+++ JAVA/String/FindAlphabet.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FindAlphabet{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[26];
+		String S = br.readLine();
+		char placeValue;
+		int index;
+		for(int i = 0; i < S.length; i++){ arr[i] = -1; }
+
+		for(int i = 0; i < S.length; i++){
+			placeValue = S.charAt(i);
+			index = (int)placeValue - 96;
+			if(arr[index] != -1) continue;
+			arr[index] = i;
+		}
+
+		for(int value : arr) { System.out.printf("%d",value); }
+	}
+}
