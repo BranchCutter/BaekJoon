0b26c4e9d8cc9df0ec93a6b3f84172e3aeaa3633
harandal24601 <devchaehokim@gmail.com>
2022-03-01 16:19:17 +0900

    feat : add algorithm BaekJoon(1065)

---
+++ JAVA/Function/Hansoo.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Hansoo{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println(arithmetic_sequence(Integer.parseInt(br.readLine())));
+	}
+
+	public static int arithmetic_sequence(int num){
+		int cnt = 0;
+
+		if(num < 100) return num;
+		else {
+			cnt = 99;
+			if(num == 1000){
+				num = 999;
+			}
+			
+			for(int i = 100; i <= num; i++){
+				int third = i / 100;
+				int second = (i / 10) % 10;
+				int first = i % 10;
+
+				if((third - second) == (second - first)) cnt++;
+			}
+		}
+		return cnt;
+	}
+}
