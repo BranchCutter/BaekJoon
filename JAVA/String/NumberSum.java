c12240737cc5f20122a03d9b9c68924c82fb85ed
harandal24601 <devchaehokim@gmail.com>
2022-03-01 16:49:25 +0900

    feat : add BaekJoon algorithm(11720)

---
+++ JAVA/String/NumberSum.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class NumberSum{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int sum = 0, counter;
+		char placeValue;
+		String firstNum = br.readLine();
+		for(int i = 0; i < N; i++){
+			placeValue = firstNum.charAt(i);
+			counter = (int)placeValue - 48;
+			sum += counter;
+		}
+		System.out.println(sum);
+	}
+}
