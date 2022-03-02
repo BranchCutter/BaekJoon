232d3315c8fec1eac6dffb53f3865de166397479
harandal24601 <devchaehokim@gmail.com>
2022-03-02 19:36:29 +0900

    feat(algorithm) : add BaekJoon algorithm(1157)

---
+++ JAVA/String/WordStudy.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class WordStudy{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int[] arr = new int[26];
+		String str = br.readLine().toUpperCase();
+
+		for(int i = 0; i < str.length(); i++){
+			arr[str.charAt(i) - 65]++;
+		}
+		
+		int max = -1;
+		char mostUsedAlphabet = '?';
+		for(int i = 0; i < 26; i++){
+			if(arr[i] > max){
+				max = arr[i];
+				mostUsedAlphabet = (char) (i + 65);
+			}
+			else if (arr[i] == max){
+				mostUsedAlphabet = '?';
+			}
+		}
+		System.out.print(mostUsedAlphabet);
+	}
+}
