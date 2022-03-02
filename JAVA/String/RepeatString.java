e4073ab6d64705ee064a7db2ae2a739e7fc45285
harandal24601 <devchaehokim@gmail.com>
2022-03-02 17:04:26 +0900

    feat(algorithm) : add BaekJoon algorithm(2675)

---
+++ JAVA/String/RepeatString.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class RepeatString{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		int testCase = Integer.parseInt(br.readLine());
+		
+		for(int i = 0; i < testCase; i++) {
+			st = new StringTokenizer(br.readLine()," ");
+			int R = Integer.parseInt(st.nextToken());
+			String S = st.nextToken();
+			for(int j = 0; j < S.length(); j++){
+				for(int k = 0; k < R; k++) { bw.write(S.charAt(j)); }
+			}
+			bw.write("\n");
+		}
+		bw.flush();
+		bw.close();
+	}
+}
+			
