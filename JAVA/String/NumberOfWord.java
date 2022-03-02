64e5b6ec0960a34e14e0132ab27c1e32c560f544
harandal24601 <devchaehokim@gmail.com>
2022-03-02 20:03:13 +0900

    feat(algorithm) : add BaekJoon algorithm(1152)

---
+++ JAVA/String/NumberOfWord.java
@@ -0,0 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NumberOfWord{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer str = new StringTokenizer(br.readLine()," ");
+		System.out.print(str.countTokens());
+	}
+}
