24302790b389092ef93077264433e369e64611b9
harandal24601 <devchaehokim@gmail.com>
2022-03-03 22:48:58 +0900

    feat(algorithm) : add BaekJoon algorithm(5622)

---
+++ JAVA/String/Dial.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Dial{
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		int cnt = 0;
+
+		for(int i = 0; i < str.length(); i++){
+			switch(str.charAt(i)){
+
+			case 'A' : case 'B' : case 'C' :
+				cnt += 3;
+				break;
+
+			case 'D' : case 'E' : case 'F' :
+				cnt += 4;
+				break;
+
+			case 'G' : case 'H' : case 'I' :
+				cnt += 5;
+				break;
+
+			case 'J' : case 'K' : case 'L' :
+				cnt += 6;
+				break;
+
+			case 'M' : case 'N' : case 'O' :
+				cnt += 7;
+				break;
+
+			case 'P' : case 'Q' : case 'R' : case 'S' :
+				cnt += 8;
+				break;
+
+			case 'T' : case 'U' : case 'V' :
+				cnt += 9;
+				break;
+
+			case 'W' : case 'X' : case 'Y' : case 'Z' :
+				cnt += 10;
+				break;
+			}
+		}
+		System.out.print(cnt);
+	}
+}
