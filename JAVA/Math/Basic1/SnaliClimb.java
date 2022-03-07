3281227c3d2ec7648973546785c0d5fc941b1955
harandal24601 <DevChaeHoKim@gmail.com>
2022-03-08 00:06:31 +0900

    feat(algorithm) : Add BaekJoon algorithm(2869)

---
+++ JAVA/Math/Basic1/SnaliClimb.java
@@ -0,0 +1,27 @@
+package JAVA.Math.Basic1;
+
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class SnaliClimb {
+    public static void main(String [] args){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int V = Integer.parseInt(st.nextToken());
+        int cnt = 0;
+
+        while(V > 0) {
+            V -= A;
+            if(V < 0) break;
+            V += B;
+            cnt++;
+        }
+
+        System.out.print(cnt);
+    }
+}
