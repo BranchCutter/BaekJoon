4130395f6d9c900ad30b4b377146334a61b25959
harandal24601 <DevChaeHoKim@gmail.com>
2022-03-08 00:29:31 +0900

    chore : add IOException

--- JAVA/Math/Basic1/SnaliClimb.java
+++ JAVA/Math/Basic1/SnaliClimb.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class SnaliClimb {
-    public static void main(String [] args){
+    public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
