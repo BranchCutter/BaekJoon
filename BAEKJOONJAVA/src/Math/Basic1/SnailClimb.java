2ea8b318a720bc885b8d3e425cd4864ff8e84139
harandal24601 <DevChaeHoKim@gmail.com>
2022-03-08 01:17:14 +0900

    feat(algorithm) : Add BaekJoon algorithm

--- BAEKJOONJAVA/src/Math/Basic1/SnailClimb.java
+++ BAEKJOONJAVA/src/Math/Basic1/SnailClimb.java
@@ -1,7 +1,7 @@
 package Math.Basic1;
 
-import java.io.IOException;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
@@ -10,18 +10,14 @@ public class SnailClimb {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int A = Integer.parseInt(st.nextToken());
-        int B = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
-        int cnt = 0;
+        int climb = Integer.parseInt(st.nextToken());
+        int slip = Integer.parseInt(st.nextToken());
+        int height = Integer.parseInt(st.nextToken());
+        int day = (height - slip) / (climb - slip);
 
-        while(V > 0) {
-            V -= A;
-            if(V < 0) break;
-            V += B;
-            cnt++;
-        }
+        if ((height - slip) % (climb - slip) != 0) day++;
 
-        System.out.print(cnt);
+        System.out.print(day);
+        
     }
 }
