b91d7b5cbea9d8335081817b4d5a44a4a123101b
harandal24601 <DevChaeHoKim@gmail.com>
2022-03-08 00:34:33 +0900

    chore : edit java file name

--- JAVA/Math/Basic1/SnailClimb.java
+++ JAVA/Math/Basic1/SnailClimb.java
@@ -5,7 +5,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class SnaliClimb {
+public class SnailClimb {
     public static void main(String [] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
