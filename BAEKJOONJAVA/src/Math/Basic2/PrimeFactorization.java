c6a0dca07e22ddd158680d2574bdad3a4900c9e5
harandal24601 <DevChaeHoKim@gmail.com>
2022-03-16 04:12:21 +0900

    feat : create BaekJoon algorithm

---
+++ BAEKJOONJAVA/src/Math/Basic2/PrimeFactorization.java
@@ -0,0 +1,31 @@
+package Math.Basic2;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class PrimeFactorization {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 2; i * i <= N; i++) {
+            while (N % i == 0) {
+                bw.write(i + "\n");
+                N /= i;
+            }
+        }
+
+        if (N != 1) {
+            bw.write(N + "");
+        }
+
+        bw.flush();
+    }
+    
+
+}
