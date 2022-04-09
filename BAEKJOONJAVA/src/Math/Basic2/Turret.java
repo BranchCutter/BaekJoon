638c2e683989acb47599a521be298e6e2b3bb1bb
harandal24601 <DevChaeHoKim@gmail.com>
2022-04-09 14:43:33 +0900

    feat : update BaekJoon algorithm(1002)

--- BAEKJOONJAVA/src/Math/Basic2/Turret.java
+++ BAEKJOONJAVA/src/Math/Basic2/Turret.java
@@ -8,19 +8,40 @@ import java.util.StringTokenizer;
 public class Turret {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
         int testCase = Integer.parseInt(br.readLine());
 
-        for(int i = 0; i < testCase; i++) {
+        while(testCase-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int X1 = Integer.parseInt(st.nextToken());
-            int Y1 = Integer.parseInt(st.nextToken());
-            int R1 = Integer.parseInt(st.nextToken());
-            int X2 = Integer.parseInt(st.nextToken());
-            int Y2 = Integer.parseInt(st.nextToken());
-            int R3 = Integer.parseInt(st.nextToken());
-
             
+            int x1 = Integer.parseInt(st.nextToken());
+            int y1 = Integer.parseInt(st.nextToken());
+            int r1 = Integer.parseInt(st.nextToken());
+
+            int x2 = Integer.parseInt(st.nextToken());
+            int y2 = Integer.parseInt(st.nextToken());
+            int r2 = Integer.parseInt(st.nextToken());
+
+            sb.append(tangent_point(x1, y1, r1, x2, y2, r2)).append('\n');
         }
+        System.out.println(sb);
+    }
+
+    public static int tangent_point(int x1, int y1, int r1, int x2, int y2, int r2) {
+
+        int distance_pow = (int)(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+
+        if(x1 == x2 && y1 == y2 && r1 == r2) return -1;
+
+        else if(distance_pow > Math.pow(r1 + r2, 2)) return 0;
+
+        else if(distance_pow < Math.pow(r2 - r1, 2)) return 0;
+
+        else if(distance_pow == Math.pow(r2 - r1, 2)) return 1;
+
+        else if(distance_pow == Math.pow(r1 + r2, 2)) return 1;
+
+        else return 2;
     }
 }
